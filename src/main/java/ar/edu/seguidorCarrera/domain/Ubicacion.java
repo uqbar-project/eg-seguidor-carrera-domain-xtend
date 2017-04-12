b0f0204975c1d37230ea86f4e2c34ebe88330c7c
pablitar <pablitar@gmail.com>
2017-04-12 20:55:20 -0300

    Modificaciones al dominio vistas en clase para poder tener un readable property en el enum

--- src/main/java/ar/edu/seguidorCarrera/domain/Ubicacion.java
+++ src/main/java/ar/edu/seguidorCarrera/domain/Ubicacion.java
@@ -1,8 +1,23 @@
 package ar.edu.seguidorCarrera.domain;
 
+import org.uqbar.commons.utils.Observable;
+
+@Observable
 public enum Ubicacion {
 	
-	PrimerCuatrimestre,
-	SegundoCuatrimestre 
+	PrimerCuatrimestre {
+		@Override
+		public String getReadableName() {
+			return "Primer Cuatrimestre";
+		}
+	},
+	SegundoCuatrimestre {
+		@Override
+		public String getReadableName() {
+			return "Segundo Cuatrimestre";
+		}
+	};
+	
+	public abstract String getReadableName();
 
 }
